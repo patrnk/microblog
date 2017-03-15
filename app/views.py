f00f0d51e2ab5713b6c66bdda6d2823e6972b70e
Paul Vergeev <paulvergeev@gmail.com>
2017-03-15 20:58:32 +0300

    Add index page template

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'nickname': 'Paul'}  # fake user
+    posts = [{'author': 'melevir', 'body': 'qweqwe'}, 
+             {'author': 'kazauwa', 'body': 'asd' }]
+    return render_template('index.html', title='title?', user=user, posts=posts)
